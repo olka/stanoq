f34718000db5ecdf4f23246cc091194373d4cc04
Alexander Kachur <alexander.kachur@gmail.com>
2017-08-23 21:57:49 +0200

    Stable version

--- src/test/scala/org/stanoq/tests/crawler/ServiceSpec.scala
+++ src/test/scala/org/stanoq/tests/crawler/ServiceSpec.scala
@@ -27,11 +27,11 @@ class ServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest with Cr
     }
   }
 
-  "CrawlerService" should "respond with >25 processed pages on crawling websocket.org with depth 3" in {
+  "CrawlerService" should "respond with >20 processed pages on crawling websocket.org with depth 3" in {
     Post(s"/crawler", ConfigProperties("https://www.websocket.org/index.html",3)) ~> crawlerService.route ~> check {
       status shouldBe OK
       contentType shouldBe `application/json`
-      responseAs[Node].getChildCount should be >25
+      responseAs[Node].getChildCount should be >20
     }
   }
 }
