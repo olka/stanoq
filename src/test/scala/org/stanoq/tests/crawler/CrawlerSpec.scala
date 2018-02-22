5b6ccb9fffa5db3962a5087d5b21c08dffff2399
Alexander Kachur <alexander.kachur@gmail.com>
2018-02-22 16:28:22 +0200

    Minor tests fix

--- src/test/scala/org/stanoq/tests/crawler/CrawlerSpec.scala
+++ src/test/scala/org/stanoq/tests/crawler/CrawlerSpec.scala
@@ -8,13 +8,12 @@ import org.stanoq.crawler.model.{ConfigProperties, Node}
 class CrawlerSpec extends FlatSpec with Matchers {
 
   "Crawler" should "handle recursive page structure" in {
-    val crawler = new Crawler(ConfigProperties("https://www.websocket.org/echo.html", 3)).process()
-    crawler.root.parse.map(println)
+    val crawler = new Crawler(ConfigProperties("https://www.websocket.org/echo.html", 2)).process()
     crawler.visitedPages.filter(url => crawler.root.print.contains(url)).size shouldBe crawler.root.convertToNode.getChildCount-1
   }
 
   "Crawler" should "properly process gatling.io" in {
-    val crawler = new Crawler(ConfigProperties("http://gatling.io", 2)).process()
+    val crawler = new Crawler(ConfigProperties("http://gatling.io", 1)).process()
     crawler.root.convertToNode.getChildCount-1 shouldBe crawler.visitedPages.size
     crawler.visitedPages.size should be >=1
   }
